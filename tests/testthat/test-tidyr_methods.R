ceb18e2f1131fd5ec718785e2a882e9645c08385
stemangiola <mangiolastefano@gmail.com>
2020-10-07 16:08:18 +1100

    test code style

--- tests/testthat/test-tidyr_methods.R
+++ tests/testthat/test-tidyr_methods.R
@@ -19,31 +19,61 @@ test_that("nest_unnest", {
 
 
     expect_equal(
-        x@int_colData@listData$reducedDims$PCA %>% as.data.frame() %>% as_tibble(rownames = "cell") %>% arrange(cell) %>% pull(PC1) %>% abs(),
-        y@int_colData@listData$reducedDims$PCA %>% as.data.frame() %>% as_tibble(rownames = "cell") %>% arrange(cell) %>% pull(PC1) %>% abs()
+        x@int_colData@listData$reducedDims$PCA %>%
+            as.data.frame() %>%
+            as_tibble(rownames = "cell") %>%
+            arrange(cell) %>%
+            pull(PC1) %>%
+            abs(),
+        y@int_colData@listData$reducedDims$PCA %>%
+            as.data.frame() %>%
+            as_tibble(rownames = "cell") %>%
+            arrange(cell) %>%
+            pull(PC1) %>%
+            abs()
     )
 })
 
 test_that("unite separate", {
     un <- tt %>% unite("new_col", c(groups, col2), sep = ":")
 
-    expect_equal(un %>% select(new_col) %>% slice(1) %>% pull(new_col), "g2:other_col")
+    un %>%
+        select(new_col) %>%
+        slice(1) %>%
+        pull(new_col) %>%
+        expect_equal("g2:other_col")
 
-    se <- un %>% separate(col = new_col, into = c("orig.ident", "groups"), sep = ":")
+    se <-
+        un %>%
+        separate(
+            col = new_col,
+            into = c("orig.ident", "groups"),
+            sep = ":"
+        )
 
-    expect_equal(se %>% select(orig.ident) %>% ncol(), 1)
+    se %>%
+        select(orig.ident) %>%
+        ncol() %>%
+        expect_equal(1)
 })
 
 test_that("extract", {
-    expect_equal(
-        tt %>% extract(groups, into = "g", regex = "g([0-9])", convert = TRUE) %>% pull(g) %>% class(),
-        "integer"
-    )
+    tt %>%
+        extract(groups,
+                into = "g",
+                regex = "g([0-9])",
+                convert = TRUE) %>%
+        pull(g) %>%
+        class() %>%
+        expect_equal("integer")
 })
 
 test_that("pivot_longer", {
-    expect_equal(
-        tt %>% pivot_longer(c(orig.ident, groups), names_to = "name", values_to = "value") %>% class() %>% .[1],
-        "tbl_df"
-    )
+    tt %>%
+        pivot_longer(c(orig.ident, groups),
+                     names_to = "name",
+                     values_to = "value") %>%
+        class() %>%
+        .[1] %>%
+        expect_equal("tbl_df")
 })
