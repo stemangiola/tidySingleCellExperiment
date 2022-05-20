e58f39619c6c31877019d1b0ce7defa0087a49e0
stemangiola <mangiolastefano@gmail.com>
2022-05-20 18:57:03 +1000

    typo

--- R/print_method.R
+++ R/print_method.R
@@ -40,7 +40,7 @@ tbl_format_header.tidySingleCellExperiment <- function(x, setup, ...){
 
       # Add further info single-cell
       append(sprintf(
-        "\033[90m Features=%s | cells=%s | Assays=%s\033[39m",
+        "\033[90m Features=%s | Cells=%s | Assays=%s\033[39m",
         number_of_features,
         nrow(x),
         assay_names %>% paste(collapse=", ")
