0413072e16d7136ac8c6775ad05de7868005ce29
HelenaLC <helena@crowells.eu>
2023-07-30 10:50:24 +0200

    simplify pipe doc

--- R/utils-pipe.R
+++ R/utils-pipe.R
@@ -1,14 +1,19 @@
-#' Pipe operator
-#'
-#' See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
-#'
 #' @name %>%
 #' @rdname pipe
 #' @keywords internal
-#' @export
-#' @importFrom magrittr %>%
+#' 
+#' @title Pipe operator
+#' @description See \code{magrittr::\link[magrittr:pipe]{\%>\%}} for details.
+#'
 #' @usage lhs \%>\% rhs
-#' @param lhs A value or the magrittr placeholder.
-#' @param rhs A function call using the magrittr semantics.
+#' @param lhs A value or the `magrittr` placeholder.
+#' @param rhs A function call using the `magrittr` semantics.
 #' @return The result of calling `rhs(lhs)`.
+#' 
+#' @examples
+#' `%>%` <- magrittr::`%>%`
+#' letters %>% head(n=3)
+#' 
+#' @importFrom magrittr %>%
+#' @export
 NULL
