2f2effe6c221da696d0b4b99841dc8fd08b1af77
William Hutchison <william.8191@fastmail.com>
2023-10-05 15:39:10 +1100

    Switch plot_ly method to S4

--- R/plotly_methods.R
+++ R/plotly_methods.R
@@ -1,76 +1,35 @@
-#' @name plotly
-#' @rdname plotly
+#' @name plot_ly
+#' @rdname plot_ly
+#' @aliases plot_ly,SingleCellExperiment-method
 #' @inherit plotly::plot_ly
 #' @return `plotly`
 #' 
-#' @examples
-#' # TODO
+#' @importFrom tidySummarizedExperiment plot_ly
 #' 
-#' @importFrom plotly plot_ly
 #' @export
-plot_ly <- function(data=data.frame(), 
-    ..., type=NULL, name=NULL,
-    color=NULL, colors=NULL, alpha=NULL,
-    stroke=NULL, strokes=NULL, alpha_stroke=1,
-    size=NULL, sizes=c(10, 100),
-    span=NULL, spans=c(1, 20),
-    symbol=NULL, symbols=NULL,
-    linetype=NULL, linetypes=NULL,
-    split=NULL, frame=NULL,
-    width=NULL, height=NULL, source="A") {
-    UseMethod("plot_ly")
-}
-
-#' @rdname plotly
-#' @export
-plot_ly.default <- function(data=data.frame(), 
-    ..., type=NULL, name=NULL,
-    color=NULL, colors=NULL, alpha=NULL,
-    stroke=NULL, strokes=NULL, alpha_stroke=1,
-    size=NULL, sizes=c(10, 100),
-    span=NULL, spans=c(1, 20),
-    symbol=NULL, symbols=NULL,
-    linetype=NULL, linetypes=NULL,
-    split=NULL, frame=NULL,
-    width=NULL, height=NULL, source="A") {
-    data %>%
-        # This is a trick to not loop the call
-        drop_class("tbl_df") %>%
-        plotly::plot_ly(...,
-            type=type, name=name,
-            color=color, colors=colors, alpha=alpha,
-            stroke=stroke, strokes=strokes, alpha_stroke=alpha_stroke,
-            size=size, sizes=sizes,
-            span=span, spans=spans,
-            symbol=symbol, symbols=symbols,
-            linetype=linetype, linetypes=linetypes,
-            split=split, frame=frame,
-            width=width, height=height, source=source)
-}
-
-#' @rdname plotly
-#' @export
-plot_ly.SingleCellExperiment <- function(data=data.frame(), 
-    ..., type=NULL, name=NULL, 
-    color=NULL, colors=NULL, alpha=NULL,
-    stroke=NULL, strokes=NULL, alpha_stroke=1,
-    size=NULL, sizes=c(10, 100),
-    span=NULL, spans=c(1, 20),
-    symbol=NULL, symbols=NULL,
-    linetype=NULL, linetypes=NULL,
-    split=NULL, frame=NULL,
-    width=NULL, height=NULL, source="A") {
-    data %>%
-        # This is a trick to not loop the call
-        as_tibble() %>%
-        plot_ly(...,
-            type=type, name=name,
-            color=color, colors=colors, alpha=alpha,
-            stroke=stroke, strokes=strokes, alpha_stroke=alpha_stroke,
-            size=size, sizes=sizes, 
-            span=span, spans=spans,
-            symbol=symbol, symbols=symbols, 
-            linetype=linetype, linetypes=linetypes,
-            split=split, frame=frame,
-            width=width, height=height, source=source)
-}
+setMethod("plot_ly", "SingleCellExperiment",
+    function(data=data.frame(), ..., 
+             type=NULL, name=NULL, 
+             color=NULL, colors=NULL, alpha=NULL,
+             stroke=NULL, strokes=NULL, alpha_stroke=1,
+             size=NULL, sizes=c(10, 100),
+             span=NULL, spans=c(1, 20),
+             symbol=NULL, symbols=NULL,
+             linetype=NULL, linetypes=NULL,
+             split=NULL, frame=NULL,
+             width=NULL, height=NULL, source="A") {
+      
+      data %>%
+        
+          # This is a trick to not loop the call
+          as_tibble() %>%
+          plot_ly(..., type=type, name=name,
+                  color=color, colors=colors, alpha=alpha,
+                  stroke=stroke, strokes=strokes, alpha_stroke=alpha_stroke,
+                  size=size, sizes=sizes, 
+                  span=span, spans=spans,
+                  symbol=symbol, symbols=symbols, 
+                  linetype=linetype, linetypes=linetypes,
+                  split=split, frame=frame,
+                  width=width, height=height, source=source)
+})
