aa9c38600708a8b2f888dee56e8bfb3c9f15a8e8
qclayssen <quentin.clayssen@gmail.com>
2023-11-19 16:21:49 +0100

    Enhance print method for SingleCellExperiment Implemented red coloring for special and needed columns in SingleCellExperiment objects. This update aligns with the requirements outlined in issue #9, enhancing the visibility and distinction of view-only columns in the printed output.

--- R/print_method.R
+++ R/print_method.R
@@ -46,20 +46,35 @@ tbl_format_header.tidySingleCellExperiment <- function(x, setup, ...) {
 #' @return Prints a message to the console describing
 #'   the contents of the `tidySingleCellExperiment`.
 #'
+#' @param x A SingleCellExperiment object.
+#' @param n (optional) The maximum number of rows to show.
+#' @param width (optional) The maximum width of the output.
 #' @examples
 #' data(pbmc_small)
 #' print(pbmc_small)
-#' 
+#'
 #' @importFrom vctrs new_data_frame
 #' @importFrom SummarizedExperiment assayNames
+#' @importFrom crayon red
+#' @importFrom dplyr mutate across all_of
 #' @export
 print.SingleCellExperiment <- function(x, ..., n=NULL, width=NULL) {
+
+    special_cols <- get_special_datasets(x) %>% 
+            map(~ .x %>% colnames()) %>% 
+            unlist() %>% 
+            unique()
+
+    # Convert to tibble and apply coloring row-wise
     x |>
         as_tibble(n_dimensions_to_return=5) |>
         new_data_frame(class=c("tidySingleCellExperiment", "tbl")) %>%
         add_attr(nrow(x), "number_of_features") %>%
-        add_attr(assayNames(x), "assay_names") %>%
-        print()
-    
+        add_attr(assayNames(x), "assay_names") |>
+        apply(1, function(row) {
+            styled_row <- ifelse(names(row) %in% special_cols, crayon::red(row), row)
+            cat(paste(styled_row, collapse = " "), "\n")
+        })
+
     invisible(x)
 }
