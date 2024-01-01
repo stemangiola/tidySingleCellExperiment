961e442e65d7f7b925243f21fba5cd27419322dc
Miha Kosmac <biomiha@gmail.com>
2024-01-01 09:37:38 +0000

    Merge plot_ly to work

--- tests/testthat/test-dplyr_methods.R
+++ tests/testthat/test-dplyr_methods.R
@@ -1,5 +1,37 @@
 library(S4Vectors)
-data(pbmc_small)
+data("pbmc_small")
+# Mock up ADT and cell hashing experiments
+set.seed(2023-08-29)
+# Antibody tags
+pos_myus <- sample(x = 7500:20000, size = 5)
+int_myus <- sample(x = 100:1500, size = 5)
+neg_myus <- sample(x = 1:30, size = 15)
+all_myus <- c(pos_myus, int_myus, neg_myus)
+all_myus <- sample(x = all_myus, size = length(all_myus))
+
+mat <- list()
+for(i in seq_along(all_myus)) {
+  mat[[i]] <-   MASS::rnegbin(n = dim(pbmc_small)[[2]], mu = all_myus[[i]], theta = all_myus[[i]]/500)
+}
+mat <- Reduce(f = cbind, x = mat)
+colnames(mat) <- paste("Ab", seq_along(mat[1,]), sep = "-")
+rownames(mat) <- colnames(pbmc_small)
+
+altExps(pbmc_small)[["ADT"]] <- SingleCellExperiment(assays = list(counts = t(mat), logcounts = log10(t(mat) + 1)))
+
+# Cell hashing
+HTO_myus <- sample(x = c(100, 100000), size = 6, replace = TRUE)
+mat <- list()
+for(i in seq_along(HTO_myus)) {
+  mat[[i]] <-   MASS::rnegbin(n = dim(pbmc_small)[[2]], mu = HTO_myus[[i]], theta = HTO_myus[[i]]/500)
+}
+
+mat <- Reduce(f = cbind, x = mat)
+colnames(mat) <- paste("HTO", seq_along(mat[1,]), sep = "-")
+rownames(mat) <- colnames(pbmc_small)
+
+altExps(pbmc_small)[["Hashtag demultiplex"]] <- SingleCellExperiment(assays = list(counts = t(mat), logcounts = log10(t(mat) + 1)))
+
 df <- pbmc_small
 df$number <- sample(seq(ncol(df)))
 df$factor <- sample(
