3b740c83f3220e1baa3f0d5dca50bbb2a62086ec
qclayssen <quentin.clayssen@gmail.com>
2024-01-11 23:46:06 +0000

    add crayon for color print

--- R/print_method.R
+++ R/print_method.R
@@ -57,22 +57,28 @@ tbl_format_header.tidySingleCellExperiment <- function(x, setup, ...) {
 #' @importFrom SummarizedExperiment assayNames
 #' @importFrom crayon red
 #' @importFrom dplyr mutate across all_of
+#' @importFrom crayon red
 #' @export
 print.SingleCellExperiment <- function(x, ..., n=NULL, width=NULL) {
+    # Retrieve special view-only columns
+    special_cols <- get_special_columns(x)
 
-    special_cols <- get_special_datasets(x) %>% 
-            map(~ .x %>% colnames()) %>% 
-            unlist() %>% 
-            unique()
+    # Convert to tibble
+    tibble_x <- x |> as_tibble(n_dimensions_to_return=5)
 
-    # Convert to tibble and apply coloring row-wise
-    x |>
-        mutate(across(all_of(special_cols), ~ crayon::red(as.character(.))))|>
-        as_tibble(n_dimensions_to_return=5) |>
-        new_data_frame(class=c("tidySingleCellExperiment", "tbl")) %>%
+    # Modify column titles for special columns
+    col_titles <- names(tibble_x)
+    modified_col_titles <- ifelse(col_titles %in% special_cols, 
+                                    crayon::red(as.character(col_titles)), 
+                                    col_titles)
+
+    names(tibble_x) <- modified_col_titles
+
+    # Print the tibble with modified column titles
+    new_data_frame(tibble_x, class=c("tidySingleCellExperiment", "tbl")) %>%
         add_attr(nrow(x), "number_of_features") %>%
-        add_attr(assayNames(x), "assay_names")|>
-        print(tbl, n = n, width = width)
+        add_attr(assayNames(x), "assay_names") %>%
+        print(n = n, width = width)
 
     invisible(x)
 }
